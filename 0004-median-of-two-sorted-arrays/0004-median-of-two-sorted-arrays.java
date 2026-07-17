872cc516bff71e013e0108b5c49902cbf6f09dae
Harshita77-hem <harshitahemnani77@gmail.com>
2026-07-17 19:29:42 +0530

    Time: 1 ms (100%), Space: 48.7 MB (87.53%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -2,31 +2,50 @@ class Solution {
     public double findMedianSortedArrays(int[] a, int[] b) {
         int n1=a.length;
         int n2=b.length;
+        int n=n1+n2;
+        int cnt=0;
         int i=0;
         int j=0;
-        int[] c=new int[n1+n2];
-        int k=0;
+       
+       
+        int idx2=n/2;
+        int idx1=idx2-1;
+        int ele1=-1;
+        int ele2=-1;
         while(i<n1 && j<n2){
-            if(a[i]<=b[j]){
-                c[k++]=a[i++];
+            if(a[i]<b[j]){
+                if(cnt==idx1) ele1=a[i];
+                if(cnt==idx2) ele2=a[i];
+                i++;
+                cnt++;
             }
             else{
-                c[k++]=b[j++];
+                if(cnt==idx1) ele1=b[j];
+                if(cnt==idx2) ele2=b[j];
+                j++;
+                cnt++;
             }
-            
+
         }
         while(i<n1){
-            c[k++]=a[i++];
+            if(cnt==idx1) ele1=a[i];
+            if(cnt==idx2) ele2=a[i];
+            i++;
+            cnt++;
+
         }
         while(j<n2){
-            c[k++]=b[j++];
+            if(cnt==idx1) ele1=b[j];
+            if(cnt==idx2) ele2=b[j];
+            j++;
+            cnt++;
         }
-        int n=n1+n2;
         if(n%2==1){
-            return c[n/2];
+            return ele2;
         }
         else{
-            return (c[n/2]+(c[(n/2)-1]))/2.0;
+            return (ele1+ele2)/2.0;
         }
+        
     }
 }
