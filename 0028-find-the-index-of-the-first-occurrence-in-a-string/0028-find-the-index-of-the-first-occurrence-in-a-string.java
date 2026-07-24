12c100cfdbb660b516b848507b4e5f2e420b6415
Harshita77-hem <harshitahemnani77@gmail.com>
2026-07-24 18:07:18 +0530

    Time: 4 ms (5.81%), Space: 43.1 MB (52.01%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,10 +1,50 @@
 class Solution {
+    public void computeLPS(String pattern,int[] lps){
+        int len=0;
+        lps[0]=0;
+        int i=1;
+        while(i<pattern.length()){
+            if(pattern.charAt(i)==pattern.charAt(len)){
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else{
+                if(len!=0){
+                    len=lps[len-1];
+                }
+                else{
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+       
+    }
     public int strStr(String haystack, String needle) {
-        int m=haystack.length();
-        int n=needle.length();
-        for(int i=0;i<=m-n;i++){
-            if(haystack.substring(i,i+n).equals(needle)){
-                return i;
+        if(needle.length()==0){
+            return 0;
+        }
+        int[] lps=new int[needle.length()];
+        computeLPS(needle,lps);
+        int i=0;
+        int j=0;
+        while(i<haystack.length()){
+            if(haystack.charAt(i)==needle.charAt(j)){
+                i++;
+                j++;
+
+            }
+            if(j==needle.length()){
+                return i-j;
+            }
+            else if(i<haystack.length() && haystack.charAt(i)!=needle.charAt(j)){
+                if(j!=0){
+                    j=lps[j-1];
+                }
+                else{
+                    i++;
+                }
             }
         }
         return -1;
