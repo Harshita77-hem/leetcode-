fb426974bd07aa9df24fe25c8aa61a865cd1d816
Harshita77-hem <harshitahemnani77@gmail.com>
2026-07-28 19:43:03 +0530

    Time: 238 ms (5.26%), Space: 47.8 MB (8.01%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String clean="";
+        for(int i=0;i<s.length();i++){
+            char ch=Character.toLowerCase(s.charAt(i));
+            if(Character.isLetterOrDigit(ch)){
+                clean+=ch;
+            }
+
+                
+
+            
+        }
+        String rev="";
+        for(int i=clean.length()-1;i>=0;i--){
+            rev+=clean.charAt(i);
+
+        }
+        return clean.equals(rev);
+        
+    }
+}
