6ec59ec1ac48c4ad4d412e30488527606543324d
Harshita77-hem <harshitahemnani77@gmail.com>
2026-07-28 22:35:26 +0530

    Time: 14 ms (26.05%), Space: 46.4 MB (34.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,24 +2,22 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()){
             return false;
-        }
-        int[] freq=new int[26];
-        for(int i=0;i<s.length();i++){
-            freq[s.charAt(i)-'a']++;
-            freq[t.charAt(i)-'a']--;
-
-            
-
 
         }
-        for(int x:freq){
-            if(x!=0){
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(char ch:t.toCharArray()){
+            if(!map.containsKey(ch)){
                 return false;
             }
+            map.put(ch,map.get(ch)-1);
+            if(map.get(ch)==0){
+                map.remove(ch);
+            }
         }
-        return true;
-
-        
+        return map.isEmpty();
         
     }
 }
