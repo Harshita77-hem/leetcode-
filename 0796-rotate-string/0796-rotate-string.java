6d5ef8ed9e52c0de33e59c439cc7d045d78db9cf
Harshita77-hem <harshitahemnani77@gmail.com>
2026-08-18 21:22:10 +0530

    Time: 0 ms (100%), Space: 43.4 MB (63.49%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        int n=s.length();
+        int m=goal.length();
+        for(int i=0;i<n;i++){
+            boolean match=true;
+            for(int j=0;j<m;j++){
+                if((s.charAt((i+j)%n)!=goal.charAt(j))){
+                    match=false;
+                    break;
+
+                }
+            }
+            if(match){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
