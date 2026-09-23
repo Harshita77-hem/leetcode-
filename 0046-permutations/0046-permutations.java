dff82d1dc7d9f34198d27f26fdfd862d2b82fab6
Harshita77-hem <harshitahemnani77@gmail.com>
2026-09-23 20:03:35 +0530

    Time: 0 ms (100%), Space: 45 MB (98.58%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,25 +1,29 @@
 class Solution {
-    public void solve(int[] nums,boolean[] freq,List<Integer> ds,List<List<Integer>> ans){
-        if(ds.size()==nums.length){
-            ans.add(new ArrayList<>(ds));
+    public static void solve(int[] nums,int index,List<List<Integer>> ans){
+        if(index==nums.length){
+            List<Integer> temp=new ArrayList<>();
+            for(int num:nums){
+                temp.add(num);
+            }
+            ans.add(temp);
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(freq[i]==false){
-                freq[i]=true;
-                ds.add(nums[i]);
-                solve(nums,freq,ds,ans);
+        for(int i=index;i<nums.length;i++){
+            int temp=nums[index];
+            nums[index]=nums[i];
+            nums[i]=temp;
 
-                ds.remove(ds.size()-1);
-                freq[i]=false;
-            }
+            solve(nums,index+1,ans);
+
+            temp=nums[i];
+            nums[i]=nums[index];
+            nums[index]=temp;
         }
+
     }
     public List<List<Integer>> permute(int[] nums) {
-        List<Integer> ds=new ArrayList<>();
         List<List<Integer>> ans=new ArrayList<>();
-        boolean[] freq=new boolean[nums.length];
-        solve(nums,freq,ds,ans);
+        solve(nums,0,ans);
         return ans;
         
     }
